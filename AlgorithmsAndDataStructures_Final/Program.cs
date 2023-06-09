cdb9f0027bac1897b8c79d42a0c085d5d3ece343
Noah <noseby@live.ca>
2023-06-09 09:47:33 -0500

    initialize dictionary of ingredients

--- AlgorithmsAndDataStructures_Final/Program.cs
+++ AlgorithmsAndDataStructures_Final/Program.cs
@@ -1,1 +1,35 @@
 ﻿
+// Algo Final Project -- Noah Miller
+
+// creating dictionary for ingredients, along with their calories
+Dictionary<string, int> ingredients = new Dictionary<string, int>
+{
+  { "Bread", 66 },
+
+  { "Ham", 72 },
+  { "Bologna", 57 },
+  { "Chicken", 17 },
+  { "Corned Beef", 53 },
+  { "Salami", 40 },
+
+  { "Cheese, American", 104 },
+  { "Cheese, Cheddar", 113 },
+  { "Cheese, Havarti", 105 },
+
+  { "Mayonnaise", 94 },
+  { "Mustard", 10 },
+  { "Butter", 102 },
+  { "Garlic Aioli", 100 },
+  { "Sriracha", 15 },
+  { "Dressing, Ranch", 73 },
+  { "Dressing, 1000 Island", 59 },
+
+  { "Lettuce", 5 },
+  { "Tomato", 4 },
+  { "Cucumber", 4 },
+  { "Banana Pepper", 10 },
+  { "Green Pepper", 3 },
+  { "Red Onion", 6 },
+  { "Spinach", 7 },
+  { "Avocado", 64 }
+};
